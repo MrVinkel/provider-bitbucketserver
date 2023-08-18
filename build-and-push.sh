3791cb37362cad2b20c1832eb220790367ac93d8
Martin Vinkel <mvv@danskecommodities.com>
2023-08-18 08:38:32 +0200

    Add script to build and push images

---
+++ build-and-push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "call with version tag i.e v1.2.3"
+fi
+
+make
+
+TAG=$1
+HASH=$(echo $HOSTNAME-$(pwd) | shasum -a 256 | cut -c1-8)
+
+docker tag build-${HASH}/provider-bitbucketserver-amd64 docker.io/vinkel/provider-bitbucketserver-amd64:$TAG
+docker tag build-${HASH}/provider-bitbucketserver-amd64 docker.io/vinkel/provider-bitbucketserver-amd64:latest
+docker tag build-${HASH}/provider-bitbucketserver-controller-amd64 docker.io/vinkel/provider-bitbucketserver-controller-amd64:$TAG
+docker tag build-${HASH}/provider-bitbucketserver-controller-amd64 docker.io/vinkel/provider-bitbucketserver-controller-amd64:latest
+
+docker push docker.io/vinkel/provider-bitbucketserver-amd64:$TAG
+docker push docker.io/vinkel/provider-bitbucketserver-amd64:latest
+docker push docker.io/vinkel/provider-bitbucketserver-controller-amd64:$TAG
+docker push docker.io/vinkel/provider-bitbucketserver-controller-amd64:latest
